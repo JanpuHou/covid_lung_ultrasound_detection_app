022e3dcd5ea2b36971c2763376fcf2555b91b95e
Janpu Hou <JanpuHou@users.noreply.github.com>
2021-07-06 18:04:15 -0700

    Update load_covid.py

--- load_covid.py
+++ load_covid.py
@@ -14,7 +14,7 @@ model = tensorflow.keras.models.load_model('covid_classification.h5')
 data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
 
 # Replace this with the path to your image
-image = Image.open('test_photo.png')
+image = Image.open('test.jpg')
 
 #resize the image to a 224x224 with the same strategy as in TM2:
 #resizing the image to be at least 224x224 and then cropping from the center
